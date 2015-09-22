a62b15e732b848646760c81c8c324d6e3c7e19ef
Gary Sieling <gary.sieling@gmail.com>
2015-09-21 20:21:26 -0400

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -4,3 +4,14 @@ CREATE AGGREGATE flatMap (anyarray)
     stype = ANYARRAY,
     initcond = '{}'
 );
+
+-- http://stackoverflow.com/questions/3994556/eliminate-duplicate-array-values-in-postgres
+CREATE OR REPLACE FUNCTION uniq (ANYARRAY) RETURNS ANYARRAY
+LANGUAGE SQL
+AS $body$
+  SELECT ARRAY(
+    SELECT DISTINCT $1[s.i]
+    FROM generate_series(array_lower($1,1), array_upper($1,1)) AS s(i)
+    ORDER BY 1
+  );
+$body$;
